3523a64bb40ad04164a51e9b9ce15ff310744d22
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-11-29 10:58:19 +0100

    enable travis ci

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :email_reports, EmailReports.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  # username: "postgres",
+  # password: "postgres",
   database: "email_reports_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
