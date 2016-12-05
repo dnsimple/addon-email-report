16e19a3d79d279a85c21bae5c37d4796078ce454
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-12-05 23:26:15 +0100

    configure database for prod

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :email_reports, EmailReports.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :email_reports, EmailReports.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
