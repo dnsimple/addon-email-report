e066b93473c314dd9a58a7e752578ebb84b067a2
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-12-08 14:13:41 +0100

    only send the report once a month

--- lib/mix/tasks/reports.send.ex
+++ lib/mix/tasks/reports.send.ex
@@ -16,10 +16,10 @@ defmodule Mix.Tasks.Reports.Send do
     Application.ensure_all_started(:dnsimple)
     Application.ensure_all_started(:swoosh)
 
-    date = DateTime.utc
+    one_month_ago = %{DateTime.utc | month: DateTime.utc.month - 1}
     Repo.all(
       from s in EmailReports.Subscription,
-       where: s.last_sent < ^date,
+       where: s.last_sent < ^one_month_ago,
        or_where: is_nil(s.last_sent),
        select: s
     )
