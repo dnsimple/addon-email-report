e690d05ee98302a404104194c405d14df45be2a1
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-12-08 14:08:33 +0100

    batch process subscriptions

---
+++ lib/mix/tasks/reports.send.ex
@@ -0,0 +1,46 @@
+defmodule Mix.Tasks.Reports.Send do
+  use Mix.Task
+
+  require Logger
+
+  alias Ecto.DateTime
+  alias EmailReports.Repo
+
+  import Mix.Ecto
+  import Ecto.Query
+
+  @shortdoc "Sends out all pending reports"
+
+  def run(_) do
+    {:ok, pid, _} = ensure_started(EmailReports.Repo, [])
+    Application.ensure_all_started(:dnsimple)
+    Application.ensure_all_started(:swoosh)
+
+    date = DateTime.utc
+    Repo.all(
+      from s in EmailReports.Subscription,
+       where: s.last_sent < ^date,
+       or_where: is_nil(s.last_sent),
+       select: s
+    )
+    |> Enum.map(&send_mail/1)
+  end
+
+  defp send_mail(%EmailReports.Subscription{} = subscription) do
+    dnsimple_account = account(subscription)
+    |> EmailReports.Dnsimple.whoami
+    |> Map.get(:account)
+
+    Logger.info "Sending mail to subscription id: #{subscription.id} (#{dnsimple_account.email})"
+
+    EmailReports.ReportEmail.simple(%{email: dnsimple_account.email, token: subscription.access_token})
+    |> EmailReports.Mailer.deliver
+  end
+
+  defp account(%EmailReports.Subscription{account_id: account_id, access_token: access_token}) do
+    %{
+      :dnsimple_access_token => access_token,
+      :dnsimple_account_id => account_id,
+    }
+  end
+end
