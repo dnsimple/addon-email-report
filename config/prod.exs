97a7c6dce53b64389d92d1db52a108b7396ec9ea
Ole Michaelis <Ole.Michaelis@googlemail.com>
2017-05-13 09:26:50 +0200

    Revert "use mailgun instead of postmark as postmark shuts down the heroku addon"
    This reverts commit bc24cf25d31613c195dd65c65c99a7ef4a5cbd87.

--- config/prod.exs
+++ config/prod.exs
@@ -67,9 +67,8 @@ config :logger, level: :info
 
 # Configure swoosh
 config :email_reports, EmailReports.Mailer,
-  adapter: Swoosh.Adapters.Mailgun,
-  api_key: System.get_env("MAILGUN_API_KEY"),
-  domain: System.get_env("MAILGUN_DOMAIN")
+  adapter: Swoosh.Adapters.Postmark,
+  api_key: System.get_env("POSTMARK_API_KEY")
 
 # Configure 3rd parties
 config :email_reports,
